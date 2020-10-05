e49624597e6da873046c6e6384c48a0b95ca1aef
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2020-10-05 16:58:04 +0300

    Set max-width for video-preview

--- src/stories/30--VideoPreview.stories.js
+++ src/stories/30--VideoPreview.stories.js
@@ -11,7 +11,7 @@ export default {
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { VideoPreview },
-  template: '<VideoPreview v-bind="$props" />'
+  template: '<VideoPreview v-bind="$props" style="max-width: 336px;" />'
 });
 
 const defaultArgs = {
